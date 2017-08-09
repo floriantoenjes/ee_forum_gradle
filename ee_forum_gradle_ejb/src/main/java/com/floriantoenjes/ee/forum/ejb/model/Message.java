1e8bff54a6fe5a603138371ae2a4f0ea5f79a62c
Florian Tönjes <skatepainter@yahoo.de>
2017-08-09 14:52:43 +0200

    Started to implement Message model

---
+++ ee_forum_gradle_ejb/src/main/java/com/floriantoenjes/ee/forum/ejb/model/Message.java
@@ -0,0 +1,83 @@
+package com.floriantoenjes.ee.forum.ejb.model;
+
+import javax.jws.soap.SOAPBinding;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+@Entity
+@Table(schema = "FORUM", name = "MESSAGE")
+public class Message implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @SequenceGenerator(
+            name = "MESSAGE_ID_GENERATOR",
+            sequenceName = "SEQ_MESSAGE",
+            allocationSize = 1,
+            initialValue = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "MESSAGE_ID_GENERATOR"
+    )
+    private Long id;
+
+    @NotNull
+    @ManyToOne
+    private User sender;
+
+    @NotNull
+    @ManyToOne
+    private User receiver;
+
+    @NotNull
+    private String subject;
+
+    @NotNull
+    private String text;
+
+    public Message() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public User getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(User receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
